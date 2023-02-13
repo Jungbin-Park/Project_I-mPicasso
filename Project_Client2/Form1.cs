33691a830c0666ecf62c84e7a55d351150180a91
Jungbin Park <=>
2023-02-13 20:15:36 +0900

    second commit

--- Project_Client2/Form1.cs
+++ Project_Client2/Form1.cs
@@ -35,6 +35,7 @@ namespace Project_Client2
         ////////////////////////그리기 부분///////////////////////////////
         
         Dictionary<string, Point> playerLocation = new Dictionary<string, Point>();
+        Dictionary<string, Point> playerLocation_End = new Dictionary<string, Point>();
         Point previousPoint = new Point();
 
         ////////////////////////그리기 부분///////////////////////////////
@@ -105,40 +106,24 @@ namespace Project_Client2
                         switch (cmd.CMD)
                         {
                             case 'P':
-                                Console.WriteLine(data);
                                 PositionPacket pp = JsonSerializer.Deserialize<PositionPacket>(data);
-                                PositionPacket_End pp_end = JsonSerializer.Deserialize<PositionPacket_End>(data);
                                 Graphics g = panel1.CreateGraphics();
                                 g.SmoothingMode = SmoothingMode.AntiAlias;
-                                Console.WriteLine("test??");
                                 if (playerLocation.ContainsKey(textBox_ID.Text))
                                 {
-                                    Console.WriteLine("test!");
-                                    g.DrawLine(Pens.Black, previousPoint.X, previousPoint.Y, pp.X, pp.Y);
-                                    previousPoint.X = pp.X; // Point
-                                    previousPoint.Y = pp.Y;
-                                }
-                                else
-                                {
-                                    playerLocation.Add(textBox_ID.Text, previousPoint);
+                                    g.DrawLine(Pens.Black, playerLocation_End[cmd.ID].X, playerLocation_End[cmd.ID].Y, pp.X, pp.Y);
+
+                                    playerLocation_End[cmd.ID] = new Point(pp.X, pp.Y);
                                 }
                                 break;
                             case 'C':
-                                Console.WriteLine(data);
                                 ClickPacket cp = JsonSerializer.Deserialize<ClickPacket>(data);
-                                ClickPacket_End cp_end = JsonSerializer.Deserialize<ClickPacket_End>(data);
                                 previousPoint = new Point();
                                 if (playerLocation.ContainsKey(textBox_ID.Text))
                                 {
-                                    previousPoint.X = cp.X; // Point
-                                    previousPoint.Y = cp.Y;
-                                }
-                                else
-                                {
-                                    playerLocation.Add(textBox_ID.Text, previousPoint);
+                                    playerLocation_End[cmd.ID] = new Point(cp.X, cp.Y);
                                 }
                                 break;
-
                         }
                     }
                 }
@@ -155,16 +140,59 @@ namespace Project_Client2
                 }
             }
         }
+
+        void SendClickPacket(bool isClick, string id, int x, int y)
+        {
+            // 서버로 ClickPacket 전송
+            ClickPacket cp = new ClickPacket();
+            cp.CMD = 'C'; // CMD
+            cp.CLICK = isClick;
+
+            cp.ID = id;
+            cp.X = x;
+            cp.Y = y;
+            string data = JsonSerializer.Serialize(cp);
+            sw.WriteLine(data);
+            sw.Flush();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("MouseDown");
+            if (e.Button == MouseButtons.Left)
+            {
+                string id;
+                id = textBox_ID.Text;  // id
+
+                if (playerLocation.ContainsKey(id))
+                {
+                    Console.WriteLine($"if/Down {id}, {e.Location}");
+                    playerLocation[id] = e.Location;
+                    SendClickPacket(true, id, playerLocation[id].X, playerLocation[id].Y);
+                }
+                else
+                {
+                    Console.WriteLine($"else/Down {id}, {e.Location}");
+                    playerLocation.Add(id, e.Location);
+                    SendClickPacket(true, id, playerLocation[id].X, playerLocation[id].Y);
+                }
+            }
+        }
     }
+    class Player
+    {
+        public string PID { get; set; }
+        public Point POS { get; set; }
+    }
+
     class CmdPacket
     {
+        public string ID { get; set; }
         public char CMD { get; set; }
     }
     class PositionPacket : CmdPacket
     {
         // CMD = 'P';
-
-        public string ID { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
 
@@ -172,7 +200,7 @@ namespace Project_Client2
     class ClickPacket : CmdPacket
     {
         public bool CLICK { get; set; }
-        public string ID { get; set; }
+        
         public int X { get; set; }
         public int Y { get; set; }
     }
@@ -181,22 +209,5 @@ namespace Project_Client2
     {
         // CMD = 'B';
         public char CHCOLOR { get; set; }
-    }
-
-    class PositionPacket_End : CmdPacket
-    {
-        // CMD = 'P';
-        public string ID { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
-    }
-
-    class ClickPacket_End : CmdPacket
-    {
-        // CMD = 'C';
-        public bool CLICK { get; set; }
-        public string ID { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
     }
 }

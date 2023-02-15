e0682ff655336bbf0d8f006a2d4d4a36cccb40ff
janjan97 <eoen482@nate.com>
2023-02-15 11:32:33 +0900

    Add] 클라이언트2 (클1에서 받아 모두에게 보여줌, 사용자별로 그려짐, 색변경, 타이머 초기화 등)

---
+++ Project_Picasso_Client_2/Project_Picasso_Client_2/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Project_Picasso_Client_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label_word = new System.Windows.Forms.Label();
+            this.label_Hint = new System.Windows.Forms.Label();
+            this.label_long = new System.Windows.Forms.Label();
+            this.label_ID = new System.Windows.Forms.Label();
+            this.button_Reset = new System.Windows.Forms.Button();
+            this.button_Answer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(172, 88);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 560);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown_1);
+            // 
+            // textBox_ID
+            // 
+            this.textBox_ID.Font = new System.Drawing.Font("굴림", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox_ID.Location = new System.Drawing.Point(26, 42);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(83, 27);
+            this.textBox_ID.TabIndex = 6;
+            this.textBox_ID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(490, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 32);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox2.Location = new System.Drawing.Point(390, 11);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(333, 32);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox3.Location = new System.Drawing.Point(812, 11);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(53, 32);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label_word
+            // 
+            this.label_word.AutoSize = true;
+            this.label_word.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_word.Location = new System.Drawing.Point(419, 56);
+            this.label_word.Name = "label_word";
+            this.label_word.Size = new System.Drawing.Size(52, 15);
+            this.label_word.TabIndex = 10;
+            this.label_word.Text = "제시어";
+            // 
+            // label_Hint
+            // 
+            this.label_Hint.AutoSize = true;
+            this.label_Hint.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_Hint.Location = new System.Drawing.Point(336, 20);
+            this.label_Hint.Name = "label_Hint";
+            this.label_Hint.Size = new System.Drawing.Size(37, 15);
+            this.label_Hint.TabIndex = 11;
+            this.label_Hint.Text = "힌트";
+            // 
+            // label_long
+            // 
+            this.label_long.AutoSize = true;
+            this.label_long.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_long.Location = new System.Drawing.Point(745, 19);
+            this.label_long.Name = "label_long";
+            this.label_long.Size = new System.Drawing.Size(55, 16);
+            this.label_long.TabIndex = 12;
+            this.label_long.Text = "글자수";
+            // 
+            // label_ID
+            // 
+            this.label_ID.AutoSize = true;
+            this.label_ID.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_ID.Location = new System.Drawing.Point(56, 19);
+            this.label_ID.Name = "label_ID";
+            this.label_ID.Size = new System.Drawing.Size(25, 20);
+            this.label_ID.TabIndex = 13;
+            this.label_ID.Text = "ID";
+            // 
+            // button_Reset
+            // 
+            this.button_Reset.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_Reset.Location = new System.Drawing.Point(851, 50);
+            this.button_Reset.Name = "button_Reset";
+            this.button_Reset.Size = new System.Drawing.Size(65, 33);
+            this.button_Reset.TabIndex = 15;
+            this.button_Reset.Text = "리셋";
+            this.button_Reset.UseVisualStyleBackColor = true;
+            this.button_Reset.Click += new System.EventHandler(this.button_Reset_Click);
+            // 
+            // button_Answer
+            // 
+            this.button_Answer.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_Answer.Location = new System.Drawing.Point(764, 50);
+            this.button_Answer.Name = "button_Answer";
+            this.button_Answer.Size = new System.Drawing.Size(81, 33);
+            this.button_Answer.TabIndex = 16;
+            this.button_Answer.Text = "정답 확인";
+            this.button_Answer.UseVisualStyleBackColor = true;
+            this.button_Answer.Click += new System.EventHandler(this.button_Answer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Project_Client2.Properties.Resources.KakaoTalk_20230214_173556069;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.button_Answer);
+            this.Controls.Add(this.button_Reset);
+            this.Controls.Add(this.label_ID);
+            this.Controls.Add(this.label_long);
+            this.Controls.Add(this.label_Hint);
+            this.Controls.Add(this.label_word);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox_ID);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox_ID;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label_word;
+        private System.Windows.Forms.Label label_Hint;
+        private System.Windows.Forms.Label label_long;
+        private System.Windows.Forms.Label label_ID;
+        private System.Windows.Forms.Button button_Reset;
+        private System.Windows.Forms.Button button_Answer;
+    }
+}
+
---
+++ Project_Picasso_Client_2/Project_Picasso_Client_2/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using System.Drawing.Drawing2D;
+using System.Data.OracleClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Media;
+
+namespace Project_Picasso_Client_2
+{
+    public partial class Form1 : Form
+    {
+        class CmdPacket
+        {
+            public string ID { get; set; }
+            public char CMD { get; set; }
+        }
+        class PositionPacket : CmdPacket
+        {
+            // CMD = 'P';
+            public int X { get; set; }
+            public int Y { get; set; }
+
+        }
+        class ClickPacket : CmdPacket
+        {
+            public bool CLICK { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+        class ColorPacket : CmdPacket
+        {
+            public int COLOR { get; set; }
+
+        }
+
+        ////////////////////////서버 부분///////////////////////////////
+        const string IP = "192.168.0.26";//"127.0.0.1";//"192.168.0.26";
+        const int PORT = 9000;
+        Socket clientSocket;
+        IPEndPoint ipep; // 서버의 접속주소
+
+        bool isRunRecv = true;
+        NetworkStream ns;
+        StreamWriter sw;
+        StreamReader sr;
+        ////////////////////////서버 부분///////////////////////////////
+
+        ////////////////////////그리기 부분///////////////////////////////
+
+        Dictionary<string, Point> playerLocation = new Dictionary<string, Point>();
+        Dictionary<string, Point> playerLocation_End = new Dictionary<string, Point>();
+
+        Dictionary<string, int> player_int = new Dictionary<string, int>();
+        Dictionary<string, Color> player_Color = new Dictionary<string, Color>();
+
+
+        ////////////////////////정답 부분///////////////////////////////
+        string word;
+        string hint;
+        string Long;
+
+        //그래픽 패널 필드
+        //Graphics g;       
+        int dr = 2;
+        //Pen pen;
+        Pen server_pen;
+        Color server_color = Color.Black;
+
+
+        ////////////////////////타이머 부분///////////////////////////////
+        int min = 0;
+        int sec = 0;
+
+        System.Windows.Forms.Timer tm = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer tm1 = new System.Windows.Forms.Timer();
+
+
+        ////////////////////////사운드 부분///////////////////////////////
+        private SoundPlayer Player = new SoundPlayer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+            this.FormClosed += Form1_FormClosed;
+
+            server_pen = new Pen(Color.Black, dr);
+
+
+            // 타이머
+            tm.Interval = 1000;
+            tm1.Interval = 1000;
+            tm.Tick += Tm_Tick;
+            tm1.Tick += Tm1_Tick;
+            this.Paint += Form1_Paint;
+
+        }
+
+        private void Tm1_Tick(object sender, EventArgs e)
+        {
+            if (textBox1.Text == word)
+            {
+                tm.Start();
+                textBox1.Visible = true;
+                textBox1.Text = "제시어가 출력되었습니다.";
+            }
+        }
+        private void Tm_Tick(object sender, EventArgs e)
+        {
+            sec++;
+            if (sec == 60)
+            {
+                min++;
+                if (min == 1)
+                {
+                    textBox2.Visible = true;
+                }
+                if (min == 2)
+                {
+                    //textBox3.Visible = true;
+                    tm.Stop();
+                }
+                sec = 0;
+
+            }
+            if(min == 1 && sec == 30)
+            {
+                textBox3.Visible = true;
+            }
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Font ft = new Font("맑은고딕", 13, FontStyle.Bold);
+            e.Graphics.DrawString($"{min}:{sec} / (제한시간 2분)",
+                ft, Brushes.Black, 180, 60);
+        }
+
+        
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.isRunRecv = false;
+            this.clientSocket.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.clientSocket = new Socket(AddressFamily.InterNetwork,
+                                        SocketType.Stream, ProtocolType.Tcp);
+            this.ipep = new IPEndPoint(IPAddress.Parse(IP), (PORT));
+            this.clientSocket.Connect(ipep);
+
+
+            this.ns = new NetworkStream(this.clientSocket);
+            this.sw = new StreamWriter(this.ns);
+            this.sr = new StreamReader(this.ns);
+
+            this.isRunRecv = true;
+            Thread threadRecv = new Thread(new ThreadStart(ThreadRecv));
+            threadRecv.IsBackground = true;
+            threadRecv.Start();
+
+            try
+            {
+                this.Player.SoundLocation = @"../../Resources/maple_BGM.wav";
+                this.Player.PlayLooping();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error playing sound");
+            }
+
+
+
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            tm1.Start();
+            textBox_ID.Text = "Admin";
+        }
+
+        void ThreadRecv()
+        {
+            while (this.isRunRecv)
+            {
+                try
+                {
+                    string data = sr.ReadLine();
+                    Console.WriteLine(data);
+
+                    if (data.Contains('|'))
+                    {
+                        string[] rdata = data.Split('|');
+                        hint = rdata[0];
+                        Long = rdata[1];
+                        word = rdata[2];
+                        textBox1.Text = word;
+                        textBox2.Text = hint;
+                        textBox3.Text = Long;
+                    }
+
+                    //Console.WriteLine($"수신 : {data}");
+                    if (data == null)
+                    {
+                        this.isRunRecv = false;
+                    }
+                    else
+                    {
+                        CmdPacket cmd = JsonSerializer.Deserialize<CmdPacket>(data);   //객체로바뀜
+
+                        switch (cmd.CMD)
+                        {
+                            case 'P':
+                                PositionPacket pp = JsonSerializer.Deserialize<PositionPacket>(data);
+                                ColorPacket colp = JsonSerializer.Deserialize<ColorPacket>(data);
+                                Graphics g = panel1.CreateGraphics();
+                                g.SmoothingMode = SmoothingMode.AntiAlias;
+                                if (playerLocation.ContainsKey(textBox_ID.Text))
+                                {
+                                    if (player_Color.ContainsKey(cmd.ID) == false)
+                                    {
+                                        g.DrawLine(Pens.Black, playerLocation_End[cmd.ID].X, playerLocation_End[cmd.ID].Y, pp.X, pp.Y);
+                                        playerLocation_End[cmd.ID] = new Point(pp.X, pp.Y);
+                                        continue;
+                                    }
+                                    else if (player_Color[cmd.ID] == Color.White)
+                                    {
+                                        Pen erase_pen = new Pen(Color.White, 20);
+                                        g.DrawLine(erase_pen, playerLocation_End[cmd.ID].X, playerLocation_End[cmd.ID].Y, pp.X, pp.Y);
+                                        playerLocation_End[cmd.ID] = new Point(pp.X, pp.Y);
+                                        continue;
+                                    }
+                                    //Console.WriteLine(player_Color[cmd.ID]);
+                                    server_pen = new Pen(player_Color[cmd.ID]);
+                                    // Console.WriteLine($"P if {player_Color[cmd.ID]}");
+                                    g.DrawLine(server_pen, playerLocation_End[cmd.ID].X, playerLocation_End[cmd.ID].Y, pp.X, pp.Y);
+                                    playerLocation_End[cmd.ID] = new Point(pp.X, pp.Y);
+                                }
+                                break;
+                            case 'C':
+                                ClickPacket cp = JsonSerializer.Deserialize<ClickPacket>(data);
+                                if (playerLocation.ContainsKey(textBox_ID.Text))
+                                {
+                                    playerLocation_End[cmd.ID] = new Point(cp.X, cp.Y);
+                                }
+                                break;
+                            case 'R':
+                                colp = JsonSerializer.Deserialize<ColorPacket>(data);
+                                if (player_Color.ContainsKey(cmd.ID))
+                                {
+                                    player_Color[cmd.ID] = ColorTranslator.FromOle(colp.COLOR);
+                                    Console.WriteLine(player_Color[cmd.ID]);
+                                    if (player_Color[cmd.ID] == Color.Empty)
+                                    {
+                                        Console.WriteLine($"R if 색이 빈 값입니다.");
+                                        server_pen = new Pen(Color.Black);  //검은색 기본값
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"R else {player_Color[cmd.ID]}");
+                                        server_pen = new Pen(player_Color[cmd.ID]);
+                                        //pen = new Pen(colp.COLOR, dr);
+                                    }
+                                }
+                                else
+                                {
+                                    Color Trans_Color = ColorTranslator.FromOle(colp.COLOR);
+                                    player_Color.Add(cmd.ID, Trans_Color);
+
+                                }
+                                break;
+                        }
+                    }
+                }
+                catch (JsonException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine($"Json Data : {ex.Data}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Recv Error :{ex.Message}");
+                    // if (ex.Source != "System.Text.Json") //
+                    this.isRunRecv = false;
+                }
+            }
+        }
+
+        void SendClickPacket(bool isClick, string id, int x, int y)
+        {
+            // 서버로 ClickPacket 전송
+            ClickPacket cp = new ClickPacket();
+            cp.CMD = 'C'; // CMD
+            cp.CLICK = isClick;
+
+            cp.ID = id;
+            cp.X = x;
+            cp.Y = y;
+            string data = JsonSerializer.Serialize(cp);
+            sw.WriteLine(data);
+            sw.Flush();
+        }
+
+        private void panel1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("MouseDown");
+            if (e.Button == MouseButtons.Left)
+            {
+                string id;
+                id = textBox_ID.Text;  // id
+
+                if (playerLocation.ContainsKey(id))
+                {
+                    Console.WriteLine($"if/Down {id}, {e.Location}");
+                    playerLocation[id] = e.Location;
+                    SendClickPacket(true, id, playerLocation[id].X, playerLocation[id].Y);
+                }
+                else
+                {
+                    Console.WriteLine($"else/Down {id}, {e.Location}");
+                    playerLocation.Add(id, e.Location);
+                    SendClickPacket(true, id, playerLocation[id].X, playerLocation[id].Y);
+                }
+            }
+        }
+
+        private void button_Reset_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button_Answer_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = word;
+            tm.Stop();
+            tm1.Stop();
+            min = 0;
+            sec = 0;
+            Invalidate();
+        }
+    }
+
+}
